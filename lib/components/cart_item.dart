e8266f84eaf33033f8f711ee1594f738aaaa4b12
wangpf1129 <wangpf2051@gmail.com>
2024-07-02 17:21:56 +0800

    Add CartItem component and update CartPage

---
+++ lib/components/cart_item.dart
@@ -0,0 +1,71 @@
+import 'package:ecommerce_app_flutter/models/shoe.dart';
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  final Shoe individualShoe;
+  final void Function() onTabToRemoveItem;
+  const CartItem(
+      {super.key,
+      required this.individualShoe,
+      required this.onTabToRemoveItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // image
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              individualShoe.imagePath,
+              width: 100,
+            ),
+          ),
+
+          // shoe details
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                individualShoe.name,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                '\$${individualShoe.price}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+
+          // remove button
+          GestureDetector(
+            onTap: onTabToRemoveItem,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(Icons.delete),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerce_app_flutter/components/cart_item.dart';
+import 'package:ecommerce_app_flutter/models/cart.dart';
+import 'package:ecommerce_app_flutter/models/shoe.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,8 +14,34 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart Page'),
-    );
+    return Consumer<Cart>(builder: (context, value, child) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(children: [
+          const Text('Cart',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              )),
+
+          const SizedBox(height: 10),
+
+          // cart items
+          Expanded(
+            child: ListView.builder(
+              itemCount: value.getUserCart().length,
+              itemBuilder: (context, index) {
+                Shoe individualShoe = value.getUserCart()[index];
+                return CartItem(
+                    individualShoe: individualShoe,
+                    onTabToRemoveItem: () {
+                      value.removeItemFromCart(individualShoe);
+                    });
+              },
+            ),
+          ),
+        ]),
+      );
+    });
   }
 }
